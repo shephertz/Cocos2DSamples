e97c750414152d28e96c4ae11309ad50f16e8e0c
Rajeev <rajeev@static-148.220.96.14-tataidc.co.in>
2014-05-22 00:46:00 +0530

    Updated Sample.

--- Fight Ninja/Fight Ninja/Utility/GameConstants.h
+++ Fight Ninja/Fight Ninja/Utility/GameConstants.h
@@ -7,14 +7,14 @@
 //
 
 
-#define APPWARP_APP_KEY     @"Your app key"
-#define APPWARP_SECRET_KEY  @"Secret"
+#define APPWARP_APP_KEY     @"cad2bfab6310acd9696187b98682925125e469ab0d0d585db0b00609f461b791"
+#define APPWARP_SECRET_KEY  @"55811709916e7ce4405cde0cdc5a254cf4b506fbafdae05760a73100b8080b67"
 
 #define GAME_NAME           @"NinjaFight"
 #define ROOM_NAME           @"R1"
 #define ROOM_OWNER          @"Rajeev"
 #define MAX_PLAYER          2
-#define GAME_OVER_TIME      30.0f //In Seconds
+#define GAME_OVER_TIME      300.0f //In Seconds
 
 #define USER_NAME           @"userName"
 #define PLAYER_POSITION     @"playerPositon"
